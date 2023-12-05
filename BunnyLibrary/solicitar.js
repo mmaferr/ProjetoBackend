8eab66641e988aeb9403318d7ac09c38c058044c
Maria Fernanda <124887919+mmaferr@users.noreply.github.com>
2023-12-05 18:29:57 -0400

    Add files via upload

---
+++ BunnyLibrary/solicitar.js
@@ -0,0 +1,12 @@
+// Função para exibir a caixa de login
+function mostrarCaixaLogin() {
+    var caixaLogin = document.getElementById("caixa-login");
+    caixaLogin.style.display = "block";
+}
+
+// Função para fechar a caixa de login
+function fecharCaixaLogin() {
+    var caixaLogin = document.getElementById("caixa-login");
+    caixaLogin.style.display = "none";
+}
+
